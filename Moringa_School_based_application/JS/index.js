df93200817b040812fdacdd9d15ff857fe09f7b0
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-29 15:59:28 +0300

    initial js commit

---
+++ Moringa_School_based_application/JS/index.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        "columnId":0,
+        "tasks":[
+            {
+                "taskId":97522,
+                "content":"Record JavaScript Intro"
+            },
+
+            {
+                "taskId":7543,
+                "content":"Moringa Lab Quiz"
+            }
+        ]
+
+    }
+]
